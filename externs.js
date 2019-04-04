ac667c1388e944055b5b8881e2cd54f6ff34fbc0
Andrey Shovkoplyas <motor4ik@gmail.com>
2019-04-04 16:49:05 +0200

    externs

---
+++ externs.js
@@ -0,0 +1,15 @@
+var Web3Utils = {};
+Web3Utils.rightPad = function(){};
+Web3Utils.leftPad = function(){};
+Web3Utils.toTwosComplement = function(){};
+Web3Utils.fromUtf8 = function(){};
+Web3Utils.bytesToHex = function(){};
+Web3Utils.numberToHex = function(){};
+Web3Utils.hexToUtf8 = function(){};
+Web3Utils.sha3 = function(){};
+
+var toBigNumber = function(){};
+
+var ethereum = {};
+ethereum.enable = function(){};
+ethereum.sendAsync = function(){};
